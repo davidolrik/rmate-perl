f6d7a5f94376cc8f3e2b0b0ac31c6c568864cc84
David Jack Wange Olrik <david@grepmasters.com>
2015-06-09 22:28:48 +0200

    We no support an optional install path argument
    - Install into $HOME by default
    - Use sudo to install if installing outside $HOME

--- install.sh
+++ install.sh
@@ -1,8 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p ~/bin
-curl -Lo ~/bin/rmate https://raw.github.com/davidolrik/rmate-perl/master/rmate
-chmod 755 ~/bin/rmate
-ln -sf ~/bin/rmate ~/bin/mate
-ln -sf ~/bin/rmate ~/bin/rsub
-ln -sf ~/bin/rmate ~/bin/subl
+INSTALL_PATH=$1
+
+# If no install path is given, install in users home directory
+if [[ -z "${INSTALL_PATH}" ]]; then
+    INSTALL_PATH="$HOME/bin"
+fi
+
+# We use bash to install with
+BASH="/bin/bash"
+
+# Use bash with sudo if installing outside users home directory
+if [[ "$INSTALL_PATH" != "$HOME"*  ]]; then
+    BASH="sudo ${BASH}"
+fi
+
+echo "Installing rmate-perl into ${INSTALL_PATH}"
+$BASH -- <<INSTALLER
+    mkdir -p "${INSTALL_PATH}"
+    curl -Lo "${INSTALL_PATH}/rmate" https://raw.github.com/davidolrik/rmate-perl/master/rmate
+    chmod 755 "${INSTALL_PATH}/rmate"
+    ln -sf "${INSTALL_PATH}/rmate" "${INSTALL_PATH}/mate"
+    ln -sf "${INSTALL_PATH}/rmate" "${INSTALL_PATH}/rsub"
+    ln -sf "${INSTALL_PATH}/rmate" "${INSTALL_PATH}/subl"
+INSTALLER
+echo "Done"
